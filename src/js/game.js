2bac58e083d3eecc3e9b7a70fbe4dcff651e1432
roshanmomand <1028086@hr.nl>
2024-05-08 00:03:05 +0200

    test2

--- src/js/game.js
+++ src/js/game.js
@@ -12,38 +12,31 @@ export class Game extends Engine {
     startGame() {
         console.log("start de game!")
 
-     
+
+
         const fish = new Actor()
-        for(let i = 0 ; i < 6 ; i++){
-            fish.graphics.use(Resources.Fish.toSprite())
-            fish.pos = new Vector(Math.floor(Math.random() * 600),Math.floor(Math.random() * 600))
-            console.log(fish.pos)
-            fish.vel = new Vector(Math.random()*50,0)
-            this.add(i)
-        }
-
-    
-        // const fish = new Actor()
-        // fish.graphics.use(Resources.Fish.toSprite())
-        // fish.pos = new Vector(800,50)
-        // fish.vel = new Vector(-40,0)
-
+        fish.graphics.use(Resources.Fish.toSprite())
+        fish.pos = new Vector(Math.floor(Math.random() * 600), Math.floor(Math.random() * 600))
+        fish.vel = new Vector(Math.random() * 50, 0)
 
         const heimer = new Actor()
         heimer.graphics.use(Resources.Heimer.toSprite())
-        heimer.pos = new Vector(0,50)
-        heimer.vel = new Vector (40,0)
+        heimer.pos = new Vector(Math.floor(Math.random() * 600), Math.floor(Math.random() * 600))
+        heimer.vel = new Vector(Math.random() *-50, 0)
 
-        const backGround = new Actor ()
+
+        const backGround = new Actor()
         backGround.graphics.use(Resources.Stars.toSprite())
-        backGround.pos = new Vector(0,0)
-        backGround.scale = new Vector(2,2)
+        backGround.pos = new Vector(0, 0)
+        backGround.scale = new Vector(2, 2)
 
 
         this.add(backGround)
         this.add(heimer)
+        this.add(fish)
+
     }
-    
+
 }
 
 new Game()
